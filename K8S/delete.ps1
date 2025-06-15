3b61c2f0fd351eb8381f0d788cd2af7f953c9fbd
Alvaro Notebook <alvarojosebaranoski01@gmail.com>
2025-06-15 13:19:01 -0300

    feat: Adds kubectl util scripts

---
+++ K8S/delete.ps1
@@ -0,0 +1,6 @@
+kubectl delete deployment platforms-depl
+kubectl delete deployment commands-depl
+
+kubectl delete service commands-clusterip-srv
+kubectl delete service platformnpservice-srv
+kubectl delete service platforms-clusterip-srv
---
+++ K8S/deploy.ps1
@@ -0,0 +1,3 @@
+ kubectl apply -f .\platforms-depl.yaml
+ kubectl apply -f .\platforms-nodeport-service.yaml
+ kubectl apply -f .\commands-depl.yaml 
